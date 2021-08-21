88ba1839f2bdd9017da311cd1b7002987210eaa3
turab45 <turabbajeer202@gmail.com>
2021-08-21 11:17:51 +0500

    create, read, update and delete in jpa

--- springboot-data-jpa/src/main/java/com/jpa/demo/SpringbootDataJpa1Application.java
+++ springboot-data-jpa/src/main/java/com/jpa/demo/SpringbootDataJpa1Application.java
@@ -1,5 +1,7 @@
 package com.jpa.demo;
 
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -15,13 +17,44 @@ public class SpringbootDataJpa1Application {
 		
 		UserRepository userRepository = context.getBean(UserRepository.class);
 		
-		UserEntity user = new UserEntity();
-		user.setName("Turab");
-		user.setCity("Islamkot");
-		user.setStatus("Programmer");
+		// Save the user
+//		UserEntity user = new UserEntity();
+//		user.setName("Turab");
+//		user.setCity("Islamkot");
+//		user.setStatus("Programmer");
+//		
+//		UserEntity entity = userRepository.save(user);
+//		System.out.println(entity);
 		
-		UserEntity entity = userRepository.save(user);
-		System.out.println(entity);
+		// update the user
+//		UserEntity user = userRepository.findById(2).get();
+//		user.setName("Ayoob");
+//		user.setCity("Nawbshah");
+//		
+//		userRepository.save(user);
+		//System.out.println(user);
+		
+		// get all
+		
+//		Iterable<UserEntity> all = userRepository.findAll();
+//		all.forEach(user->{
+//			System.out.println(user);
+//		}
+//		);
+//		
+		
+		// delete the record by id
+		
+		userRepository.deleteById(2);
+		
+		System.out.println("Deleted....");
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
