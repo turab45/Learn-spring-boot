37b25a264253577d8eaae7b42bc3d09529b6e94e
turab45 <turabbajeer202@gmail.com>
2021-08-24 10:02:38 +0500

    upload image to a dynamic path

--- springboot-restapi/src/main/java/com/restapi/controller/FileUploadController.java
+++ springboot-restapi/src/main/java/com/restapi/controller/FileUploadController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.restapi.helper.FileUploadHelper;
 
@@ -27,7 +28,7 @@ public class FileUploadController {
 		boolean uploaded = fileUploadHelper.uploadFile(file);
 		
 		if(uploaded)
-			return ResponseEntity.ok().body("File has been uploaded successfully.");
+			return ResponseEntity.ok().body("File has been uploaded successfully. View at: "+ServletUriComponentsBuilder.fromCurrentContextPath().path("/images/").path(file.getOriginalFilename()).toUriString());
 		else
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong!");
 	}
--- springboot-restapi/src/main/java/com/restapi/helper/FileUploadHelper.java
+++ springboot-restapi/src/main/java/com/restapi/helper/FileUploadHelper.java
@@ -1,18 +1,30 @@
 package com.restapi.helper;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileUploadHelper {
-
-	private static final String UPLOAD_DIR = "F:\\Tuition\\WEB\\Spring Boot Workspace\\springboot-restapi\\src\\main\\resources\\static\\images";
 	
+	// This is hard coded, we need an absolute path.
+	//private static final String UPLOAD_DIR = "F:\\Tuition\\WEB\\Spring Boot Workspace\\springboot-restapi\\src\\main\\resources\\static\\images";
+
+	private static String UPLOAD_DIR = null;
+	
+	
+	public FileUploadHelper() throws IOException{
+		UPLOAD_DIR = new ClassPathResource("static/images").getFile().getAbsolutePath();
+	}
+
+
+
 	public boolean uploadFile(MultipartFile file) {
 		boolean uploaded = false;
 		
