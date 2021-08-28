b012e474593ad7069c1c62d7e77aa656b6ac5ec2
turab45 <turabbajeer202@gmail.com>
2021-08-28 12:37:23 +0500

    add side nav

--- smart-contact-manager/src/main/java/com/scm/controller/AdminController.java
+++ smart-contact-manager/src/main/java/com/scm/controller/AdminController.java
@@ -1,6 +1,9 @@
 package com.scm.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 	
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model, Principal principal) {
+		String name = principal.getName();
+		System.out.println(name);
 		return "admin/home";
 	}
 }
--- smart-contact-manager/src/main/java/com/scm/controller/UserController.java
+++ smart-contact-manager/src/main/java/com/scm/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.scm.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +13,10 @@ public class UserController {
 	
 	
 	@GetMapping("/dashboard")
-	public String dashboard() {
+	public String dashboard(Model model, Principal principal) {
+		
+		String name = principal.getName();
+		System.out.println(name);
 		return "normal/index";
 	}
 }
