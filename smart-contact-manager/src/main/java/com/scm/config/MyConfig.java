611ea3b63723b92fba36cb2e427d66d63b3cadd1
turab45 <turabbajeer202@gmail.com>
2021-08-28 10:28:29 +0500

    customize login page

--- smart-contact-manager/src/main/java/com/scm/config/MyConfig.java
+++ smart-contact-manager/src/main/java/com/scm/config/MyConfig.java
@@ -45,7 +45,7 @@ public class MyConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
 		.antMatchers("/user/**").hasRole("NORMAL")
-		.antMatchers("/**").permitAll().and().formLogin().and().csrf().disable();
+		.antMatchers("/**").permitAll().and().formLogin().loginPage("/signin").and().csrf().disable();
 		super.configure(http);
 	}
 	
--- smart-contact-manager/src/main/java/com/scm/controller/HomeController.java
+++ smart-contact-manager/src/main/java/com/scm/controller/HomeController.java
@@ -79,4 +79,9 @@ public class HomeController {
 
 		return "register";
 	}
+	
+	@GetMapping("/signin")
+	public String login() {
+		return "login";
+	}
 }
