ac5d21eb02aabc7f37b8a6d0cef181733272d374
turab45 <turabbajeer202@gmail.com>
2021-08-28 08:39:30 +0500

    add security config

--- smart-contact-manager/src/main/java/com/scm/dao/UserRepository.java
+++ smart-contact-manager/src/main/java/com/scm/dao/UserRepository.java
@@ -1,12 +1,10 @@
 package com.scm.dao;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.scm.entities.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer>{
 	
-	public List<UserEntity> findByEmail(String email);
+	public UserEntity findByEmail(String email);
 }
--- smart-contact-manager/src/main/java/com/scm/entities/UserEntity.java
+++ smart-contact-manager/src/main/java/com/scm/entities/UserEntity.java
@@ -3,12 +3,14 @@ package com.scm.entities;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
@@ -20,6 +22,7 @@ public class UserEntity {
 	private Integer id;
 	@NotBlank(message = "User name can't be null.")
 	private String name;
+	@Column(unique = true)
 	@Email(regexp = "^[A-Za-z0-9+_.-]+@(.+)$", message = "Must be a well formed email address.")
 	private String email;
 	@NotBlank(message = "Please provide a suitable password.")
