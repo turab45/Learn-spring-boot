6ed55596cfe7ce020ff6b32ed36a01888f2c4673
turab45 <turabbajeer202@gmail.com>
2021-09-01 10:41:13 +0500

    update contact

--- smart-contact-manager/src/main/java/com/scm/controller/UserController.java
+++ smart-contact-manager/src/main/java/com/scm/controller/UserController.java
@@ -9,6 +9,7 @@ import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,33 +37,34 @@ public class UserController {
 	private UserRepository userRepository;
 	@Autowired
 	private ContactRepository contactRepository;
-	
+
 	@GetMapping("/dashboard")
 	public String dashboard(Model model, Principal principal) {
-		
+
 		String name = principal.getName();
 		System.out.println(name);
 		model.addAttribute("title", "User Dashboard");
 		return "normal/index";
 	}
-	
-	
+
 	// Show the contact form
 	@GetMapping("/add-contact")
 	public String addContact(Model model, Principal principal) {
-		
+
 		String name = principal.getName();
 		System.out.println(name);
-		
+
 		model.addAttribute("title", "Add New Contact");
 		model.addAttribute("contact", new ContactEntity());
 		return "normal/add_contact";
 	}
-	
+
 	// Add a new contact
-	
+
 	@PostMapping("/process-contact")
-	public String processContact(@Valid @ModelAttribute("contact") ContactEntity contactEntity, BindingResult result, @RequestParam("lname") String lastName,@RequestParam("profileImg") MultipartFile file, Model model, Principal principal) {
+	public String processContact(@Valid @ModelAttribute("contact") ContactEntity contactEntity, BindingResult result,
+			@RequestParam("lname") String lastName, @RequestParam("profileImg") MultipartFile file, Model model,
+			Principal principal) {
 		if (result.hasErrors()) {
 			System.out.println(result);
 			model.addAttribute("title", "Add New Contact");
@@ -70,133 +72,123 @@ public class UserController {
 			return "normal/add_contact";
 		}
 		try {
-			
+
 			if (!file.isEmpty()) {
 				contactEntity.setImage(file.getOriginalFilename());
-				
+
 				File saveFile = new ClassPathResource("static/img").getFile();
-				
-				
-				Path path = Paths.get(saveFile.getAbsolutePath()+File.separator+file.getOriginalFilename());
-				
+
+				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
+
 				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-				
-			}else {
+
+			} else {
 				contactEntity.setImage("default.jpg");
 			}
-			
-			
-			
+
 			UserEntity userEntity = userRepository.findByEmail(principal.getName());
-			
-			contactEntity.setName(contactEntity.getName()+" "+lastName);
+
+			contactEntity.setName(contactEntity.getName() + " " + lastName);
 			contactEntity.setUser(userEntity);
-			
+
 			contactRepository.save(contactEntity);
-			
+
 			model.addAttribute("alertClass", "alert-success");
 			model.addAttribute("message", "Contact has been added successfully.");
 			model.addAttribute("contact", new ContactEntity());
-			
+
 		} catch (Exception e) {
 			model.addAttribute("alertClass", "alert-danger");
 			model.addAttribute("message", "Something went wrong.");
 		}
 		return "normal/add_contact";
 	}
-	
-	
-	
+
 	@GetMapping("/contacts")
 	public String viewContacts(Model model, Principal principal) {
-		
+
 		model.addAttribute("title", "User Contacts");
 		UserEntity userEntity = userRepository.findByEmail(principal.getName());
 		List<ContactEntity> userContacts = contactRepository.findContactsByUser(userEntity.getId());
-		
+
 		model.addAttribute("contacts", userContacts);
 		return "normal/view_contacts";
 	}
-	
+
 	@GetMapping("/contact/{id}")
 	public String viewSingleContact(@PathVariable("id") Integer id, Model model) {
 		ContactEntity contactEntity = contactRepository.findById(id).get();
-		
+
 		model.addAttribute("title", contactEntity.getName());
 		model.addAttribute("contact", contactEntity);
 		return "normal/view_single_contact";
 	}
-	
-	
+
 	@GetMapping("/contact/delete/{id}")
 	public String deleteContact(@PathVariable("id") Integer id, Model model) {
 		ContactEntity contactEntity = contactRepository.findById(id).get();
-		
+
 		contactRepository.delete(contactEntity);
-		
+
 		model.addAttribute("alertClass", "alert-success");
 		model.addAttribute("message", "Your contact has been deleted.");
-		
+
 		return "redirect:/user/contacts";
 	}
-	
+
 	@GetMapping("/contact/update/{id}")
 	public String updateContact(@PathVariable("id") Integer id, Model model) {
 		ContactEntity contactEntity = contactRepository.findById(id).get();
-		
+
 		model.addAttribute("title", contactEntity.getName());
 		model.addAttribute("contact", contactEntity);
 		if (contactEntity.getName().split(" ").length > 1) {
 			model.addAttribute("fname", contactEntity.getName().split(" ")[0]);
 			model.addAttribute("lname", contactEntity.getName().split(" ")[1]);
-		}
+		} else
+			model.addAttribute("fname", contactEntity.getName());
 		return "normal/update_contact";
 	}
-	
-	
+
 	// update-contact
-	
-	@PostMapping("/{id}/update-contact")
-	public String updateContact(@Valid @ModelAttribute("contact") ContactEntity contactEntity, BindingResult result, @RequestParam("lname") String lastName,@RequestParam("profileImg") MultipartFile file, Model model, Principal principal) {
-		if (result.hasErrors()) {
-			System.out.println(result);
-			model.addAttribute("title", "Add New Contact");
-			model.addAttribute("contact", contactEntity);
-			return "normal/add_contact";
-		}
+
+	@PostMapping("/{id}/update")
+	public String updateContact(@ModelAttribute("contact") ContactEntity contactEntity,
+			@RequestParam("lname") String lastName, @RequestParam("profileImg") MultipartFile file, HttpSession session, Principal principal, Model model) {
+
 		try {
-			
+			ContactEntity oldContact = contactRepository.findById(contactEntity.getId()).get();
+
 			if (!file.isEmpty()) {
 				contactEntity.setImage(file.getOriginalFilename());
-				
+
 				File saveFile = new ClassPathResource("static/img").getFile();
-				
-				
-				Path path = Paths.get(saveFile.getAbsolutePath()+File.separator+file.getOriginalFilename());
-				
+
+				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
+
 				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-				
-			}else {
-				contactEntity.setImage("default.jpg");
-			}
-			
-			
-			
+
+			}else
+				contactEntity.setImage(oldContact.getImage());
+
 			UserEntity userEntity = userRepository.findByEmail(principal.getName());
-			
-			contactEntity.setName(contactEntity.getName()+" "+lastName);
+
+			contactEntity.setName(contactEntity.getName() + " " + lastName);
 			contactEntity.setUser(userEntity);
-			
+
+			System.out.println("Contact : " + contactEntity);
 			contactRepository.save(contactEntity);
+
+			session.setAttribute("alertClass", "alert-success");
+			session.setAttribute("message", "Contact has been added successfully.");
 			
-			model.addAttribute("alertClass", "alert-success");
-			model.addAttribute("message", "Contact has been added successfully.");
-			model.addAttribute("contact", new ContactEntity());
-			
+
 		} catch (Exception e) {
 			model.addAttribute("alertClass", "alert-danger");
 			model.addAttribute("message", "Something went wrong.");
 		}
-		return "normal/add_contact";
+
+		System.out.println("ENDING UPDATE ...");
+		return "redirect:/user/contact/"+contactEntity.getId();
 	}
 }
